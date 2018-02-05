51a0d42324d6ff2fc3187e9b8e273e6baba342e4
amiecroteau <amiecroteau@gmail.com>
2018-02-05 17:26:13 -0500

    Made adjustments to You Like Meat

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-alert("Hello World");
 // JavaScript Document
+
+var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat){
+	
+	document.write("you like meat");
+	
+} else{
+	
+	document.write("you hate meat");
+}
